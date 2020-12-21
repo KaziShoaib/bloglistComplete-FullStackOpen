ea25e3854c2d3b849393266af670bee0d9737e10
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-21 15:42:28 +0600

    added firrst batch of practice test with jest

---
+++ utils/list_helper.js
@@ -0,0 +1,67 @@
+const _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0)
+    return {};
+  const maxLike = Math.max(...blogs.map(blog => blog.likes));
+  const blogWithMaxLike = blogs.find(blog => blog.likes === maxLike);
+  return {
+    title: blogWithMaxLike.title,
+    author: blogWithMaxLike.author,
+    likes: blogWithMaxLike.likes
+  };
+};
+
+const mostBlogs = (blogs) => {
+  if(blogs.length === 0)
+    return {};
+  const authorWithCount = _.countBy(blogs, blog => blog.author);
+  const authors = _.keys(authorWithCount);
+  const blogCounts = _.values(authorWithCount);
+  const maxCount = Math.max(...blogCounts);
+  const maxIndex = _.indexOf(blogCounts, maxCount);
+  const result = { author: authors[maxIndex], blogs: maxCount };
+  return result;
+};
+
+const mostLikes = (blogs) => {
+  if(blogs.length === 0){
+    return {};
+  }
+
+  const blogsGroupedByAuthor = _.groupBy(blogs, blog => blog.author);
+  const authors = _.keys(blogsGroupedByAuthor);
+  const blogGroups = _.values(blogsGroupedByAuthor);
+
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  const authorTotalLikes = blogGroups.map(blogs => blogs.reduce(reducer, 0));
+  const maxTotalLike = Math.max(...authorTotalLikes);
+  const maxIndex = _.indexOf(authorTotalLikes, maxTotalLike);
+
+  return {
+    author: authors[maxIndex],
+    likes: maxTotalLike
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+};
