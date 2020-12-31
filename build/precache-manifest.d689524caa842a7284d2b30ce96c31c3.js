247762e4098f39dbbaba6a2f43343f3aa13fbfe3
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-31 10:17:23 +0600

    fixed build

---
+++ build/precache-manifest.d689524caa842a7284d2b30ce96c31c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3018aeb8b3bb4b2ae2d4a30824a82e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53309a699bd7861115f1",
+    "url": "/static/css/main.bdd9568e.chunk.css"
+  },
+  {
+    "revision": "d9c6bbf02a55d0ad2ada",
+    "url": "/static/js/2.1c0ebdd1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1c0ebdd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53309a699bd7861115f1",
+    "url": "/static/js/main.14162916.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
